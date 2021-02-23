1b832296dab619019a839b534ff0ede2d56089f1
Jonjoe Whitfield <me@jonjoe.io>
2021-02-23 00:30:42 +0000

    fixes

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Countdown from 'react-countdown';
+
+import logoImage from "./assets/logo.png"
+import motifImage from "./assets/motif.png"
+
+import 'bootstrap/dist/css/bootstrap.css';
+
+import "./index.css"
+
+const countdownRenderer = (props: any) => {
+  const { hours, minutes, seconds, completed, days } = props
+
+  if (completed) {
+    // Render a completed state
+    return <span>Jonjoe forgot to add a finish state!</span>;
+  } else {
+    // Render a countdown
+    return <div className="date">
+      <span>{days}</span>
+      <span>{hours}</span>
+      <span>{minutes}</span>
+      <span>{seconds}</span>
+    </div>
+  }
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div className="wrapper" style={{backgroundImage: `url(${motifImage})`}}>
+      <img className="logo" src={logoImage} alt="Warcom Logo" />
+      
+      <div className="date">
+        <Countdown renderer={countdownRenderer} date={Date.parse("28 Mar 2021 00:12:00 GMT")}/>
+      </div>
+        <span>Until Airsoft baby!!!!</span>
+    </div>
   </React.StrictMode>,
   document.getElementById('root')
 );
